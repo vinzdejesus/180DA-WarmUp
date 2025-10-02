22d5b75521967ac5b1083eb4dcf2b062c0b73f03
vinzdejesus <dejesusvinzwendell@gmail.com>
2025-10-01 19:13:56 -0700

    Camera Exercises

--- opencv_practice.py
+++ opencv_practice.py
@@ -1,19 +1,13 @@
-## [imports]
 import cv2 as cv
 import sys
-## [imports]
-## [imread]
+
 img = cv.imread(cv.samples.findFile("starry_night.jpeg"))
-## [imread]
-## [empty]
+
 if img is None:
     sys.exit("Could not read the image.")
-## [empty]
-## [imshow]
+
 cv.imshow("Display window", img)
 k = cv.waitKey(0)
-## [imshow]
-## [imsave]
+
 if k == ord("s"):
     cv.imwrite("starry_night.png", img)
-## [imsave]
